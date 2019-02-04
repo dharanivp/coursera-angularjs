eb87e148d160c1bcf2c515e7049538280299b3c8
Prakash <dharani.prakash@honeywell.com>
2019-02-04 21:08:49 +0530

    My Module-1 assignment

---
+++ module-1-solution/app.js
@@ -0,0 +1,38 @@
+(function () {
+'use strict';
+
+angular.module('myLunchCheckApp', [])
+.controller('MyLunchCheckController',MyLunchCheckController);
+
+MyLunchCheckController.$inject = [$scope];
+function MyLunchCheckController($scope)
+{
+	$scope.dishes = "";
+	$scope.DisplayMessage = "hello";
+	$scope.count=0;
+	$scope.checkItems = function()
+	{
+		if($scope.dishes.length > 0)
+	    {
+		var array =  $scope.dishes.split(',');
+        $scope.count = array.length;
+		}
+		else
+		{
+			$scope.count=0;
+		}
+		
+		if($scope.count <= 3 && $scope.count > 0)
+			$scope.DisplayMessage = "Enjoy!";
+		else if($scope.count > 3)
+			$scope.DisplayMessage = "Too much!";
+		else
+			$scope.DisplayMessage = "Please enter data first";
+			
+		return $scope.DisplayMessage;
+	};
+	console.log($injector.annotate(MyLunchCheckController));
+	
+}
+
+})();
